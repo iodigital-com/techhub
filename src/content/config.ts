676bdb5e897a8596c3bbffbbc53baa3b4a7061ea
Bram Groothedde <bramgroothedde@gmail.com>
2023-12-28 11:45:48 +0100

    add authors and reference authors in articles

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const articlesCollection = defineCollection({
   type: "content",
@@ -7,13 +7,29 @@ const articlesCollection = defineCollection({
     date: z.string(),
     images: z.array(z.string()).default([]),
     summary: z.string(),
-    authors: z.array(z.string()),
+    authors: z.array(reference("authors")),
     theme: z.string().optional(),
     serie: z.string().optional(),
     canonicalUrl: z.string().optional(),
   }),
 });
 
+const authorsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    name: z.string(),
+    avatar: z.string().optional().nullable(),
+    occupation: z.string().optional().nullable(),
+    twitter: z.string().optional().nullable(),
+    linkedin: z.string().optional().nullable(),
+    github: z.string().optional().nullable(),
+    website: z.string().optional().nullable(),
+    medium: z.string().optional().nullable(),
+    email: z.string().optional().nullable(),
+  }),
+});
+
 export const collections = {
   articles: articlesCollection,
+  authors: authorsCollection,
 };
