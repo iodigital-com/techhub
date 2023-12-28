0f2d1a625c2b1080d59374d0d1bdb78da2a1428b
Bram Groothedde <bramgroothedde@gmail.com>
2023-12-28 11:41:23 +0100

    remove non-markdown files

--- src/content/headerNavLinks.js
+++
@@ -1,8 +0,0 @@
-const headerNavLinks = [
-  { href: '/articles', title: 'Articles' },
-  { href: '/videos', title: 'Videos' },
-  { href: '/talks', title: 'Talks' },
-  { href: '/experiments', title: 'Experiments' },
-]
-
-export default headerNavLinks
