980f3755463fb7b136b13ec00a7530593848ea71
Ravindre Ramjiawan <ravindreramjiawan@hotmail.com>
2024-01-03 15:58:35 +0100

    RSS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,11 +1,23 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss'
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
 
 export async function GET(context: any) {
+  const articles = await getCollection("articles");
+
+  const items = articles
+    .sort((a, b) => Date.parse(b.data.date) - Date.parse(a.data.date))
+    .map(({ data: { date, title, summary }, slug }) => ({
+      title,
+      description: summary,
+      link: `/articles/${slug}`,
+      pubDate: new Date(date),
+    }));
+
   return rss({
     title: 'TechHub | Blog',
     description: 'TechHub',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./content/articles/**/*.md')),
+    items,
     customData: `<language>en-us</language>`,
   })
 }
