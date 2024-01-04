0a453a06df3210339a5ea07e7b9fb1679fd43b8e
dsirobokov@anwb.nl <dsirobokov@anwb.nl>
2024-01-04 15:52:22 +0100

    resuse authors grid component inside article cards

--- src/components/ArticleReact.jsx
+++ src/components/ArticleReact.jsx
@@ -1,25 +1,12 @@
+import ContributorsGrid from './ContributorsGrid';
+
 function ArticleReact({ article, authors }) {
   const url = '/articles/' + article.slug + '/'
 
   return (
     <div className="grid grid-cols-12 my-10 bg-slate-200 p-5 rounded-lg">
-      <header className="col-span-5">
-        <ul className="flex gap-10 items-center">
-          {authors.map(({ data }) => (
-            <li className="flex gap-2 flex-col items-center">
-              {data.avatar && (
-                <img
-                  src={data.avatar.src}
-                  alt={data.name}
-                  width={150}
-                  height={150}
-                  className="rounded-full"
-                />
-              )}
-              <h2>{data.name}</h2>
-            </li>
-          ))}
-        </ul>
+      <header className="col-span-5 col-start-2 content-start">
+        <ContributorsGrid contributors={authors} />
       </header>
       <article className="col-start-7 col-span-full flex gap-5 flex-col">
         <h1 className="text-xl">
