be4181a43e59d56726b37664d5c523f499ae4668
Zjerilon <zjerilon@Zjerilons-MacBook-Pro.local>
2024-01-15 10:53:42 +0100

    Fixed types

--- src/utils/remark-reading-time.ts
+++ src/utils/remark-reading-time.ts
@@ -2,7 +2,7 @@ import getReadingTime from 'reading-time';
 import { toString } from 'mdast-util-to-string';
 
 export function remarkReadingTime() {
-  return function(tree, { data }) {
+  return function(tree: object, { data }: { data: any }) {
     const textOnPage = toString(tree);
     const readingTime = getReadingTime(textOnPage);
     // readingTime.text will give us minutes read as a friendly string,
