c2fbd225b656367fc5f104f03806ece743a162bb
Ravindre Ramjiawan <ravindreramjiawan@hotmail.com>
2024-01-15 12:09:27 +0100

    Add scripts

---
+++ src/scripts/jobs.js
@@ -0,0 +1,46 @@
+const parser = require('xml2js');
+
+const fs = require('fs');
+const path = require('path');
+
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+const getJobs = async () => {
+  const baseURL = 'https://www.iodigital.com/api/job-feed';
+
+  const {
+    offers: { offer: offers },
+  } = await fetch(baseURL)
+    .then((res) => res.text())
+    .then((res) => parser.parseStringPromise(res, { explicitArray: false }))
+    .catch((err) => console.error(err));
+
+  const jobs = offers
+    .map((offer) => ({
+      id: offer.id,
+      title: offer.title.trim(),
+      location: offer.location,
+      country: offer.country,
+      city: offer.city,
+      postal_code: offer.postal_code,
+      country_code: offer.country_code,
+      department: offer.department,
+      employment_type: offer.employment_type_code,
+      category: offer.category,
+      experience: offer.experience_code,
+      education: offer.education_code,
+      tags: offer.tags,
+      min_hours: parseInt(offer.min_hours),
+      max_hours: parseInt(offer.max_hours),
+      careers_url: offer.careers_url,
+      created_at: new Date(offer.created_at.at(0)),
+      updated_at: new Date(offer.updated_at.at(0)),
+      published_at: new Date(offer.published_at.at(0)),
+      closed: offer.close_at,
+    }))
+    .filter((job) => job.department === 'Technology');
+
+  fs.writeFileSync(path.resolve('data/jobs.json'), JSON.stringify({ jobs }));
+};
+
+getJobs();
