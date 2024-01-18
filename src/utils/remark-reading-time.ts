937f460b44052386af728c5d2c738e09aed38e11
Ravindre Ramjiawan <ravindreramjiawan@hotmail.com>
2024-01-18 11:29:58 +0100

    Add Astro Lottie integration Improve read time

--- src/utils/remark-reading-time.ts
+++ src/utils/remark-reading-time.ts
@@ -4,9 +4,6 @@ import { toString } from 'mdast-util-to-string';
 export function remarkReadingTime() {
   return function (tree: object, { data }: { data: any }) {
     const textOnPage = toString(tree);
-    const readingTime = getReadingTime(textOnPage);
-    // readingTime.text will give us minutes read as a friendly string,
-    // i.e. "3 min read"
-    data.astro.frontmatter.minutesRead = readingTime.text;
+    data.astro.frontmatter.readingTime = getReadingTime(textOnPage);
   };
 }
